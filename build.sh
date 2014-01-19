c69d51419bf1ea4f024bf35206557fbf90ec79c7
Callan Barrett <callan@zoocar.org>
2014-01-19 17:53:44 +0800

    Moved back build scripts

--- build.sh
+++ build.sh
@@ -5,10 +5,10 @@
 [[ -d build/ ]] && rm -rf build/
 mkdir build/
 
-for f in $(ls ../starcheat/*.py); do
+for f in $(ls starcheat/*.py); do
 	cp $f build/
 done
 
-for t in $(ls ../starcheat/templates/*.ui); do
+for t in $(ls starcheat/templates/*.ui); do
 	pyuic5 $t > build/qt_$(basename $t | tr '[:upper:]' '[:lower:]' | sed 's/ui$/py/')
 done
