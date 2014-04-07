05f748a7d971216e165eb5f49ab18255418b50b4
Callan Barrett <callan@zoocar.org>
2014-04-07 15:20:38 +0800

    Bump version, get ready for release

--- starcheat/config.py
+++ starcheat/config.py
@@ -16,7 +16,7 @@ else:
 if os.path.isdir(config_folder) == False:
     os.mkdir(config_folder)
 
-STARCHEAT_VERSION = "0.11 (Enraged Koala)"
+STARCHEAT_VERSION = "0.12 (Enraged Koala)"
 CONFIG_VERSION = 7
 ini_file = os.path.join(config_folder, "starcheat.ini")
 
