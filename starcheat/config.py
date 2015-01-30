26fa778ef28c0eec55ff74885c61f1e4896f1363
Christian Moritz <chrmoritz@gmail.com>
2015-01-30 21:01:00 +0100

    improved starbound auto detect:
    * added register uninstall location (support custom steam libraries)

--- starcheat/config.py
+++ starcheat/config.py
@@ -90,6 +90,14 @@ class Config():
         if platform.system() == "Windows":
             import winreg
             try:
+                starbound_uninstall = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE,
+                    "SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Steam App 211820")
+                starbound_path = winreg.QueryValueEx(starbound_uninstall, "InstallLocation")[0]
+                known_locations.append(os.path.normpath(starbound_path))
+                starbound_uninstall.Close()
+            except OSError:
+                pass
+            try:
                 steam = winreg.OpenKey(winreg.HKEY_CURRENT_USER, "Software\Valve\Steam")
                 steam_path = os.path.normpath(winreg.QueryValueEx(steam, "SteamPath")[0])
                 known_locations.append(os.path.join(steam_path, "SteamApps", "common", "Starbound"))
