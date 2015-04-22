3ef590fdaa7a9d73955c57f183da768b3319376b
Callan Barrett <callan@zoocar.org>
2015-04-23 01:36:31 +0800

    Prepare for 0.22
    - Tested for Spirited Giraffe
    - Add basic quests management
    - Support for showing text colours
    - Temporarily disable random name gen
    - Various GUI tweaks and bug fixes

--- starcheat/config.py
+++ starcheat/config.py
@@ -16,8 +16,8 @@ else:
 if not os.path.isdir(config_folder):
     os.mkdir(config_folder)
 
-STARCHEAT_VERSION = "0.21 (Upbeat Giraffe)"
-STARCHEAT_VERSION_TAG = "0.21"
+STARCHEAT_VERSION = "0.22 (Spirited Giraffe)"
+STARCHEAT_VERSION_TAG = "0.22"
 CONFIG_VERSION = 14
 ini_file = os.path.join(config_folder, "starcheat.ini")
 
