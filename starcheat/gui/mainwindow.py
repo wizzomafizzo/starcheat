c999c451198572a73aff8819c7706ef7783b0a90
Callan Barrett <callan@zoocar.org>
2015-04-25 13:28:57 +0800

    Remove name gen stuff, change import/export

--- starcheat/gui/mainwindow.py
+++ starcheat/gui/mainwindow.py
@@ -126,7 +126,6 @@ class MainWindow():
         self.ui.health_button.clicked.connect(lambda: self.max_stat("health"))
         self.ui.energy_button.clicked.connect(lambda: self.max_stat("energy"))
 
-        #self.ui.name_button.clicked.connect(self.random_name)
         self.ui.copy_uuid_button.clicked.connect(self.copy_uuid)
 
         self.window.setWindowModified(False)
@@ -668,11 +667,6 @@ class MainWindow():
         self.player.set_gender(self.get_gender())
         self.update_player_preview()
         self.set_edited()
-
-    def random_name(self):
-        species_name = self.player.get_race()
-        name = self.assets.species().generate_name(species_name)
-        self.ui.name.setText(name)
 
     def set_game_mode(self):
         self.player.set_game_mode(self.assets.player().get_mode_type(self.ui.game_mode.currentText()))
